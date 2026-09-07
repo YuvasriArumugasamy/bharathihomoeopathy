8b35b9d41898e8cd7d11aa56797c9211fd78847d
yuvasri <yuvasrikutty2005@gmail.com>
2026-09-07 14:43:48 +0530

    Fix AdminSidebar to be fixed on the left viewport

--- src/components/admin/AdminSidebar.jsx
+++ src/components/admin/AdminSidebar.jsx
@@ -53,7 +53,7 @@ export const AdminSidebar = ({ isOpen, onClose }) => {
       )}
 
       <aside
-        className={`fixed lg:sticky top-0 left-0 z-50 h-screen w-64 bg-navy-950 text-slate-300 flex flex-col border-r border-navy-900 transition-transform duration-300 ${
+        className={`fixed top-0 left-0 z-50 h-screen w-64 bg-navy-950 text-slate-300 flex flex-col border-r border-navy-900 transition-transform duration-300 ${
           isOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'
         }`}
       >
--- src/layouts/AdminLayout.jsx
+++ src/layouts/AdminLayout.jsx
@@ -16,7 +16,7 @@ export const AdminLayout = () => {
       />
 
       {/* Main Admin Content Wrapper */}
-      <div className="flex-1 flex flex-col min-w-0 w-full max-w-full overflow-x-hidden">
+      <div className="flex-1 flex flex-col min-w-0 w-full max-w-full lg:pl-64 overflow-x-hidden transition-all">
         <AdminTopbar onToggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
         
         <main className="flex-1 p-4 sm:p-6 lg:p-8 max-w-[1600px] w-full mx-auto overflow-x-hidden">
